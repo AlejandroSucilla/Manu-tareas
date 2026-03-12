5815ed67ad5c37cd84cf6e5ccd3966ba0f5e7041
asucilla <asucilla@institutmvm.cat>
2026-03-12 15:29:26 +0100

    ejercicio 3 laravel

---
+++ app/Http/Controllers/PeliculaControler.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pelicula;
+use Illuminate\Http\Request;
+
+class PeliculaControler extends Controller
+{
+    //
+    public function index()
+    {
+        // Equivalent a: SELECT * FROM llibres
+        $totsPelicules = Pelicula::all();
+
+        // Enviem les dades a la vista (com el ModelAndView)
+        return view('pelicula.index', ['Pelicula' => $totsPelicules]);
+    }
+    public function create(){
+        return view('pelicula.create');
+    }
+    public function store(\Illuminate\Http\Request $request)
+    {
+        // 1. Creem un objecte nou del nostre Model (com una fila buida a la taula)
+        $nouPelicula = new \App\Models\Pelicula();
+
+        // 2. Omplim cada camp amb el que l'usuari ha escrit al formulari.
+        // Fem servir $request->input('NOM_DEL_CAMP_HTML')
+        $nouPelicula->titulo = $request->input('titulo');
+        $nouPelicula->pais = $request->input('pais');
+        $nouPelicula->año_estreno = $request->input('año_estreno');
+        $nouPelicula->nominaciones_oscar = $request->input('nominaciones_oscar');
+        $nouPelicula->oscar_ganados = $request->input('oscar_ganados');
+
+        // 3. El mètode save() l'envia definitivament a la base de dades MySQL
+        $nouPelicula->save();
+
+        // 4. Finalment, tornem al llistat de llibres per veure que s'ha afegit correctament
+        return redirect('/mostrar');
+    }
+
+
+}
